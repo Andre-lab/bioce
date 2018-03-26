4da8cff3c5a54c5a8a13881b454ba57e6053999b
wpotrzebowski <Wojciech.Potrzebowski@esss.se>
2018-03-26 21:15:25 +0200

    residual plots added

--- make_plot.py
+++ make_plot.py
@@ -37,7 +37,7 @@ def make_plot(data, data_name, log=False):
 
     intensities = data[:,1]
     line1, = plt.plot(qvector, intensities, '-o')
-    #plt.legend(handles=[line1])
+    plt.legend(handles=[line1])
     #if log:
     #    plt.semilogy(qvector, intensities, 'g-', linewidth=1.5)
     #else:
@@ -51,14 +51,41 @@ def make_plot(data, data_name, log=False):
     plt.savefig(data_name+".png", dpi=600)
     plt.show()
 
-def make_residue_plot(data, log=False):
+def make_bar_plot(data, data_name):
     """
-    Making residual plot
+    Creatiing bar chart
+    :param data:
+    :param data_name:
+    :return:
+    """
+    xvalues = [1,3,5]
+    heights = [0.424346, 0.54757, 0.476754]
+    widhts = [1.8, 1.8, 1.8]
+    fig, ax = plt.subplots(1, 1)
+    plt.bar(xvalues, heights, widhts)
+    ax.set_ylabel("Model Evidence")
+    ax.set_xticks(xvalues)
+    ax.set_xticklabels(['2models', '3models', '4models'])
+    fig.savefig(data_name+".png", dpi=600)
+    plt.show()
+def make_residue_plot(data, combined_data, data_name, log=False):
+    """
+    Making residual plot. Difference between experimental and simulated curves
+    Scaling factor needs to be applied first
+    Can be compared with teh experimental error
     :param data:
     :param log:
     :return:
     """
-
+    qvector = data[:,0]
+    exp_intensities = data[:,1]
+    sim_intensities = combined_data
+    line1, = plt.plot(qvector, exp_intensities - sim_intensities, 'o')
+    #plt.legend(handles=[line1])
+    plt.ylabel("Residuals")
+    plt.xlabel("$q[\AA]$")
+    plt.savefig(data_name+".png", dpi=600)
+    plt.show()
 if __name__=="__main__":
     doc = """
         Python interface to produce sacttering plots.
@@ -73,9 +100,13 @@ if __name__=="__main__":
 
     parser.add_option("-d", "--data", dest="data_file", default=None,
                       help="Data file containing qvec, intensities [OBLIGATORY]")
+    parser.add_option("-c", "--cmb_data", dest="combined_data_file", default=None,
+                      help="Data file containing qvec, intensities [OBLIGATORY]")
     options, args = parser.parse_args()
 
     data_file = options.data_file
+    combined_data_file = options.combined_data_file
     data = read_file_safe(data_file)
-    make_plot(data, data_file)
+    combined_data = read_file_safe(combined_data_file)
+    make_residue_plot(data, combined_data, data_file)
 
