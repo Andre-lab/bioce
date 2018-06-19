54fd8f0a6451cbf61c85bedde6b6a4ddbf33e7ea
wpotrzebowski <Wojciech.Potrzebowski@esss.se>
2018-06-19 21:26:35 +0200

    added handling for differebt darwin versions

--- setup.py
+++ setup.py
@@ -1,6 +1,34 @@
+"""
+Setup script for distutils currently working with UNIX based systemes
+
+"""
 # setup.py
 import distutils
+import sys
+import os
 from distutils.core import setup, Extension
+
+is_64bits = sys.maxsize > 2**32
+enable_openmp = False
+if sys.platform == 'darwin':
+    if not is_64bits:
+        # Disable OpenMP
+        enable_openmp = False
+    else:
+        # Newer versions of Darwin don't support openmp
+        try:
+            darwin_ver = int(os.uname()[2].split('.')[0])
+            if darwin_ver >= 12:
+                enable_openmp = False
+        except:
+            print("PROBLEM determining Darwin version")
+
+extensions = [Extension('_vbwSC', ["vbw_sc.i", "VBW_sc.cpp"],
+                             swig_opts=["-I/usr/include/python2.7"],
+                             extra_compile_args= ["-shared", "-fpic", "-fopenmp",
+                                              "-O3", "-std=c+11" ],
+                             libraries =[ "lgsl", "lgslcblas", "lm"]
+                             )]
 
 setup(name = "bioce",
       version = "1.0",
@@ -9,12 +37,7 @@ setup(name = "bioce",
       author_email='wojciech.potrzebowski@esss.se',
       url='https://www.andrelab.org',
       ext_package='vbwSC',
-      ext_modules=[Extension('_vbwSC', ["vbw_sc.i", "VBW_sc.cpp"],
-                             swig_opts=["-I/usr/include/python2.7"],
-                             extra_postargs= ["-shared", "-fpic", "-fopenmp",
-                                              "-O3", "-std=c+11" ],
-                             libraries =[ "lgsl", "lgslcblas", "lm"]
-                             )],
+      ext_modules=extensions,
       py_modules=['psis','psisloo','stan_models','stan_utility',
                   'statistics','variationalBayesian','fullBayesian']
       )
