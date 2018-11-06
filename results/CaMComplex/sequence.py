25904f6d4fa2a4429425810304be7b8ba605a77e
Wojtek Potrzebowski <wojciech.potrzebowski@esss.se>
2018-11-06 09:44:07 +0100

    removed results file to make repository light-weight

--- results/CaMComplex/sequence.py
+++
@@ -1,5 +0,0 @@
-from Bio import SeqIO
-handle = open("trans_1CLL_full_noCA_0031_894.pdb", "rU")
-for record in SeqIO.parse(handle, "pdb-seqres") :
-    print (">" + record.id + "\n" + record.seq)
-handle.close()
