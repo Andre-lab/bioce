b50d805cb19f49143b6383cca9e8975acf14a95a
Wojtek Potrzebowski <wojciech.potrzebowski@esss.se>
2019-04-29 11:33:23 +0200

    Simplified VBI interface

--- variationalBayesian.py
+++ variationalBayesian.py
@@ -8,8 +8,10 @@ __author__ = "Wojtek Potrzebowski"
 __maintainer__ = "Wojtek Potrzebowski"
 __email__ = "Wojciech.Potrzebowski@biochemistry.lu.se"
 
+import numpy as np
 import optparse
 import vbwSC
+import os
 
 def produce_final_output(output, file_list):
     #1. Match file list with weight list
@@ -34,6 +36,22 @@ def produce_final_output(output, file_list):
             logfile.write(structure_file_list[index]+"\n")
     logfile.close()
 
+def read_file_safe(filename, skip_lines, dtype="float64"):
+    """
+    Simple check if file exists
+    :param filename:
+    :return:
+    """
+    try:
+        results = np.genfromtxt(filename, dtype=dtype, skip_header=skip_lines)
+    except IOError as err:
+        print(os.strerror(err.errno))
+    return results
+
+def extract_parameters(filename):
+    data = read_file_safe(filename)
+    return np.shape(data)
+
 if __name__=="__main__":
     doc = """
         Python interface to Variational Bayesian algorithm
@@ -44,35 +62,20 @@ if __name__=="__main__":
     option_parser_class = optparse.OptionParser
     parser = option_parser_class( usage = usage, version='0.1' )
 
-    parser.add_option("-r", "--restart", dest="restart",default = 0,
-                      type = 'int',
-                      help="Restart or not")
-    parser.add_option("-n", "--number_of_strcutures", dest="nstruct",default = None,
-                      type = 'int',
-                      help="Number of strcutural model [OBLIGATORY]")
-    parser.add_option("-m", "--measures", dest="measures",default = None,
-                      type = 'int',
-                      help="Number of measurements on which algorithm is evaluated [OBLIGATORY]")
-    parser.add_option("-M", "--cs_measures", dest="cs_measures",default = None,
-                      type = 'int',
-                      help="Number of chemical shifts measurements  [OBLIGATORY]")
-    parser.add_option("-v", "--number_of_curves", dest="ncurves",default = 1,
-                      type = 'int',
-                      help="Number of scattering curves [OBLIGATORY]")
-    parser.add_option("-p", "--priors", dest="priors",
-                      help="Prior weights [OBLIGATORY]")
-    parser.add_option("-a", "--strcuture_energies", dest="structure_energies", default="None",
-                      help="Energies of strcutures used to setup priors")
     parser.add_option("-s", "--simulated", dest="simulated",
                       help="Simulated SAXS curves [OBLIGATORY]")
     parser.add_option("-e", "--experimental", dest="experimental",
                       help="Experimental SAXS curves [OBLIGATORY]")
     parser.add_option("-S", "--cs_simulated", dest="cs_simulated", default="None",
-                      help="Simulated CS data [OBLIGATORY]")
+                      help="Simulated CS data")
     parser.add_option("-E", "--cs_experimental", dest="cs_experimental", default="None",
-                      help="Experimental CS data [OBLIGATORY]")
+                      help="Experimental CS data")
     parser.add_option("-R", "--cs_rms", dest="cs_rms", default="None",
-                      help="RMS of simulated CS data [OBLIGATORY]")
+                      help="RMS of simulated CS data")
+    parser.add_option("-p", "--priors", dest="priors",
+                      help="Prior weights [OBLIGATORY]")
+    parser.add_option("-P", "--strcuture_energies", dest="structure_energies", default="None",
+                      help="Energies of strcutures used to setup priors")
     parser.add_option("-o", "--output", dest="output",
                       help="Output file [OBLIGATORY]")
     parser.add_option("-c", "--cores", dest="nprocs",default = 1,
@@ -86,9 +89,22 @@ if __name__=="__main__":
                       help="Skipping VBW step goes to model evidence directly")
     parser.add_option("-f", "--file_list", dest="file_list",
                       help="List of structure files")
+    parser.add_option("-r", "--restart", dest="restart", default=0,
+                      type='int',
+                      help="Restart or not after equilibration")
     options, args = parser.parse_args()
-    vbwSC.run_vbw(options.restart, options.nstruct, options.priors, options.structure_energies,\
-                  options.measures, options.cs_measures, options.simulated, options.ncurves, options.experimental,\
+
+    #In this version there is only one curve taken into consideration
+    ncurves = 1
+    (number_of_structures, number_of_measures) = extract_parameters(options.simulated)
+    if options.cs_experimental:
+        (number_of_cs_structures, number_of_cs_measures) = extract_parameters(options.cs_simulated)
+        if number_of_cs_structures != number_of_structures:
+            raise('Number of saxs and nmr simulated curves differ')
+    else:
+        number_of_cs_measures = 0
+    vbwSC.run_vbw(options.restart, number_of_structures, options.priors, options.structure_energies,\
+                  number_of_measures, number_of_cs_measures, options.simulated, ncurves, options.experimental,\
                   options.output, options.nprocs, options.weight_cut, options.skip_vbw,\
                   options.cs_simulated, options.cs_rms, options.cs_experimental)
 
