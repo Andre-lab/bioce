5de9d84f55820e3b960aa57e3ad31e7ce379c053
Wojtek Potrzebowski <wojciech.potrzebowski@esss.se>
2020-09-04 07:59:38 +0200

    first running unoptimized version

--- variationalBayesianPy.py
+++ variationalBayesianPy.py
@@ -9,10 +9,9 @@ __maintainer__ = "Wojtek Potrzebowski"
 __email__ = "Wojciech.Potrzebowski@biochemistry.lu.se"
 
 import numpy as np
-from scipy.special import gamma, digamma
+from scipy.special import gammaln, digamma
 from scipy import optimize
 import optparse
-#import vbwSC
 import os
 
 
@@ -28,19 +27,13 @@ def read_file_safe(filename, dtype="float64"):
         print(os.strerror(err.errno))
     return results
 
-def produce_final_output(output, file_list):
+def produce_final_output(output, weights, Lfunc, file_list):
     #1. Match file list with weight list
     logfile = open(output+".log","w")
-    output_lines = open(output).readlines()
-    weights = output_lines[-6].split(" ")[:-4]
-    output_JSD = output_lines[-5]
-    output_chi2 = output_lines[-3]
-    output_ModelEvidence = output_lines[-1]
 
     logfile.write("Log file from variational Bayesian algorithm\n")
-    logfile.write(output_chi2)
-    logfile.write(output_JSD)
-    logfile.write(output_ModelEvidence)
+    #logfile.write(output_chi2)
+    logfile.write(str(Lfunc)+'\n')
 
     logfile.write("Models selected:\n")
     structure_file_list = open(file_list).readlines()[0].strip("\n").split(" ")
@@ -48,7 +41,7 @@ def produce_final_output(output, file_list):
         raise Exception("Weights and file list have different size!")
     for index, weight in enumerate(weights):
         if float(weight)>0.0:
-            logfile.write(structure_file_list[index]+"\n")
+            logfile.write(structure_file_list[index]+" : "+str(weight)+"\n")
     logfile.close()
 
 def read_file_safe(filename, skip_lines, dtype="float64"):
@@ -63,24 +56,21 @@ def read_file_safe(filename, skip_lines, dtype="float64"):
         print(os.strerror(err.errno))
     return results
 
-def extract_parameters(filename):
-    data = read_file_safe(filename,0)
-    return np.shape(data)
 
 def setup_constants(simulated, errors):
     """
 
     :return:
     """
-    (number_of_measurements, number_of_structures) = extract_parameters(simulated)
+    (number_of_measurements, number_of_structures) = np.shape(simulated)
 
-    gamma_over_lhalf = 1.0/gamma(0.5*number_of_measurements)
-    gamma_half = gamma(0.5)
+    log_gamma_lhalf = gammaln(0.5*number_of_structures)
+    log_gamma_half = gammaln(0.5)
 
     #Matrix with i,j,k indexes
-    mixed_terms = np.zeros(number_of_structures,
+    mixed_terms = np.zeros((number_of_structures,
                            number_of_structures,
-                           number_of_measurements)
+                           number_of_measurements))
     #TODO: Do smarter summation here to avoid iterations over the loop
     #TODO: Errors need to have proper shape (reshape may be used).
     for i in range(number_of_structures):
@@ -88,38 +78,41 @@ def setup_constants(simulated, errors):
             for k in range(number_of_measurements):
                 mixed_terms[i,j,k] = simulated[k,i]*simulated[k,j]\
                                      /(errors[k]**errors[k])
-    return gamma_over_lhalf, gamma_half, mixed_terms
+    return log_gamma_lhalf, log_gamma_half, mixed_terms
 
 def Lfunction(alphas, *params):
     """
 
     :return:
     """
-    experimental, errors, intensities, gamma_over_lhalf, gamma_half, mixed_terms = params
+    intensities, errors, simulated, log_gamma_lhalf, log_gamma_half, mixed_terms = params
+    (number_of_measurements, number_of_structures) = np.shape(simulated)
     term1 = 0.0
     term2 = 0.0
     term3 = 0.0
     alpha_zero = np.sum(alphas)
+
     for alpha in alphas:
-        term1 += np.log(gamma_half/gamma(alpha))
+        term1 += log_gamma_half - gammaln(alpha)
         #TODO: This lime can be optimizied
         term1 += (alpha - 0.5)*(digamma(alpha)-digamma(alpha_zero))
-    for i in range(intensities):
-        for j, alpha in enumerate(alphas):
-            term2 += np.pow(experimental - intensities[i,j]*alpha/alpha_zero)
-    term2 = 0.5*term2/errors
 
-    for i in range(alphas):
-        for j in range(alphas):
+    for i, intensity in enumerate(intensities):
+        for j, alpha in enumerate(alphas):
+            term2 += np.power(intensity - simulated[i,j]*alpha/alpha_zero,2)/np.power(errors[j],2)
+    term2 = 0.5*term2
+
+    for i in range(number_of_structures):
+        for j in range(number_of_structures):
             alpha_i = alphas[i]
             alpha_j = alphas[j]
-            for k in range(intensities):
+            for k in range(number_of_measurements):
                 if i == j:
                     term3 += mixed_terms[i,j,k] * (alpha_i * (alpha_zero - alpha_i) - alpha_i * alpha_j)
                 else:
-                    term3 += mixed_terms[i, j, k] * (alpha_zero**2*(alpha_zero + 1))
+                    term3 += mixed_terms[i,j,k] * (alpha_zero**2*(alpha_zero + 1))
     term3 = 0.5 * term3
-    Lfunc = np.log(gamma_over_lhalf*gamma(alpha_zero)) + term1 + term2 + term3
+    Lfunc = log_gamma_half - gammaln(alpha_zero) + term1 + term2 + term3
     return Lfunc
 
 def simulated_annealing(simulated, priors, intensities, errors, gamma_over_lhalf, gamma_half, mixed_terms):
@@ -128,11 +121,16 @@ def simulated_annealing(simulated, priors, intensities, errors, gamma_over_lhalf
     :return:
     """
 
+    number_of_structures = np.shape(simulated)[1]
     params = intensities, errors, simulated, gamma_over_lhalf, gamma_half, mixed_terms
     alphas0 = priors
-    res = optimize.anneal(Lfunction, alphas0, args=params, schedule='boltzmann',
-                          full_output=True, maxiter=500, lower=-10,
-                          upper=10, dwell=250, disp=True)
+
+    lw = [-1e7] * number_of_structures
+    up = [1e7] * number_of_structures
+    res = optimize.dual_annealing(Lfunction, x0=alphas0, args=params, maxiter=100, bounds=list(zip(lw, up)), seed=1234,
+                                  no_local_search=True, initial_temp=5.e4)
+    weights = res.x/sum(res.x)
+    return weights, res.fun
 
 def run_variational(simulated_file, priors_file, experimental_file, output_file, file_list, weight_cut):
     """
@@ -147,20 +145,21 @@ def run_variational(simulated_file, priors_file, experimental_file, output_file,
     """
 
 
-    experimental = read_file_safe(experimental_file)
+    print("Reading files")
+    experimental = read_file_safe(experimental_file,0)
     intensities = experimental[:, 1]
     errors = experimental[:,2]
-    simulated = read_file_safe(simulated_file)
-    priors = read_file_safe(priors_file)
+    simulated = read_file_safe(simulated_file,0)
+    priors = read_file_safe(priors_file,0)
 
     gamma_over_lhalf, gamma_half, mixed_terms = setup_constants(simulated, errors)
 
     #TODO: Write to the same format or may them talk to each other
     #TODO: Here we need a loop for model selection
-    simulated_annealing(simulated, priors, intensities, errors,
+    weights, Lfunc = simulated_annealing(simulated, priors, intensities, errors,
                         gamma_over_lhalf, gamma_half, mixed_terms)
 
-    produce_final_output(output_file, file_list)
+    produce_final_output(output_file, weights, Lfunc, file_list)
 
 
 if __name__=="__main__":
